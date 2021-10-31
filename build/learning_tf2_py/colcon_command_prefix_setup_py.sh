192df110aac16feb7aadc5a432ecf20d663abbd9
Arturs Elksnis <arturs@elksnis.co.uk>
2021-10-31 01:25:45 +0100

    Tidying up

--- build/learning_tf2_py/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/student5/robotics/ROS_experiments/dev_ws/install/turtlesim/share/turtlesim/package.sh"
--- build/turtlesim/turtlesim__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student5/robotics/ROS_experiments/dev_ws/build/turtlesim/turtlesim__py
-# Build directory: /home/student5/robotics/ROS_experiments/dev_ws/build/turtlesim/turtlesim__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
